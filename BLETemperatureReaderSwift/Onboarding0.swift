5ce3d8e46a035b4d23c9e1b5f5e04e7744ed54c3
Michelle <jiz520@lehigh.edu>
2018-04-19 02:38:16 -0400

    Add several onboarding pages

---
+++ BLETemperatureReaderSwift/Onboarding0.swift
@@ -0,0 +1,35 @@
+//
+//  Onboarding0.swift
+//  BLETemperatureReaderSwift
+//
+//  Created by 钟佳耘 on 4/19/18.
+//  Copyright © 2018 Cloud City. All rights reserved.
+//
+
+import UIKit
+
+class Onboarding0: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
